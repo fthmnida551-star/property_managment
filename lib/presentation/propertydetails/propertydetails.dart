489fc6579fa5fa5a015235569ea46dc49bd1cbc9
unknown <fthmnida551@gmail.com>
2025-10-07 14:09:45 +0530

    faiha code

--- lib/presentation/propertydetails/propertydetails.dart
+++ lib/presentation/propertydetails/propertydetails.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:property_managment/core/theme/app_colors.dart';
 import 'package:property_managment/core/theme/asset_resource.dart';
+import 'package:property_managment/widget/green_button.dart';
 
 class PropertydetailsScreen extends StatefulWidget {
   const PropertydetailsScreen({super.key});
   @override
   State<PropertydetailsScreen> createState() => _PropertydetailsScreenState();
 }
+
 class _PropertydetailsScreenState extends State<PropertydetailsScreen> {
   @override
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
@@ -20,10 +23,7 @@ class _PropertydetailsScreenState extends State<PropertydetailsScreen> {
                 SizedBox(
                   width: double.infinity,
                   height: 250,
-                  child: Image.asset(
-                    AssetResource.property,
-                    fit: BoxFit.cover,
-                  ),
+                  child: Image.asset(AssetResource.property, fit: BoxFit.cover),
                 ),
                 Padding(
                   padding: EdgeInsets.symmetric(horizontal: 14, vertical: 14),
@@ -41,7 +41,7 @@ class _PropertydetailsScreenState extends State<PropertydetailsScreen> {
                   right: 0,
                   child: Container(
                     padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                    decoration:  BoxDecoration(
+                    decoration: BoxDecoration(
                       color: AppColors.whitecolor,
                       borderRadius: BorderRadius.only(
                         topLeft: Radius.circular(30),
@@ -66,30 +66,24 @@ class _PropertydetailsScreenState extends State<PropertydetailsScreen> {
                       color: AppColors.black,
                     ),
                   ),
-                   SizedBox(height: 10),
-                 Text(
+                  SizedBox(height: 10),
+                  Text(
                     'Modern Amenities\n2 BHK - 2 Bathroom - 1380 Sqft',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: AppColors.black,
-                    ),
+                    style: TextStyle(fontSize: 16, color: AppColors.black),
                   ),
-                SizedBox(height: 15),
+                  SizedBox(height: 15),
                   // --- Location ---
                   Row(
                     children: [
                       Icon(Icons.location_on, color: AppColors.black),
-                     SizedBox(width: 5),
+                      SizedBox(width: 5),
                       Text(
                         'KARAVATTOM, MALAPPURAM',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: AppColors.black,
-                        ),
+                        style: TextStyle(fontSize: 15, color: AppColors.black),
                       ),
                     ],
                   ),
-                   SizedBox(height: 16),
+                  SizedBox(height: 16),
                   // --- Styled ExpansionTiles Section ---
                   ExpansionTileTheme(
                     data: ExpansionTileThemeData(
@@ -103,8 +97,14 @@ class _PropertydetailsScreenState extends State<PropertydetailsScreen> {
                       ),
                       backgroundColor: Colors.white,
                       collapsedBackgroundColor: Colors.white,
-                      tilePadding: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-                      childrenPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                      tilePadding: EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 4,
+                      ),
+                      childrenPadding: EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 8,
+                      ),
                       iconColor: Colors.black,
                       collapsedIconColor: Colors.black,
                     ),
@@ -121,7 +121,7 @@ class _PropertydetailsScreenState extends State<PropertydetailsScreen> {
                             ),
                           ),
                           children: [
-                         Padding(
+                            Padding(
                               padding: EdgeInsets.only(bottom: 8.0),
                               child: Align(
                                 alignment: Alignment.centerLeft,
@@ -140,27 +140,45 @@ class _PropertydetailsScreenState extends State<PropertydetailsScreen> {
                               child: Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                   Row(
-                                    children: [                         
-                                  Icon(Icons.apartment, color: AppColors.black),
-                                  Text('  Flats/Apartments'),],),
+                                  Row(
+                                    children: [
+                                      Icon(
+                                        Icons.apartment,
+                                        color: AppColors.black,
+                                      ),
+                                      Text('  Flats/Apartments'),
+                                    ],
+                                  ),
                                   Divider(thickness: 2),
                                   Row(
                                     children: [
-                                  Icon(Icons.check_circle_outline, color: AppColors.black),
-                                  Text('  Ready to move'),],),
+                                      Icon(
+                                        Icons.check_circle_outline,
+                                        color: AppColors.black,
+                                      ),
+                                      Text('  Ready to move'),
+                                    ],
+                                  ),
                                   Divider(thickness: 2),
                                   Row(
                                     children: [
-                                  Icon(Icons.account_circle, color: AppColors.black),
-                                  Text('  Owner'),],)
+                                      Icon(
+                                        Icons.account_circle,
+                                        color: AppColors.black,
+                                      ),
+                                      Text('  Owner'),
+                                    ],
+                                  ),
                                 ],
                               ),
                             ),
-                         SizedBox(height: 8),
-                         Text(
+                            SizedBox(height: 8),
+                            Text(
                               'Property Details',
-                              style: TextStyle(fontSize: 20, color: AppColors.black),
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: AppColors.black,
+                              ),
                             ),
                             SizedBox(height: 8),
                             // Property Details Box
@@ -169,69 +187,111 @@ class _PropertydetailsScreenState extends State<PropertydetailsScreen> {
                                 border: Border.all(color: AppColors.black),
                                 borderRadius: BorderRadius.circular(8),
                               ),
-                              child:  Column(
+                              child: Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
                                   Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                     children: [
-                                      Icon(Icons.bed, color: AppColors.black),
-                                      Text(' BHK'),
-                                      SizedBox(width: 250),                         
-                                      Text('2'),
+                                      Row(
+                                        children: [
+                                          Icon(Icons.bed, color: AppColors.black),
+                                          Text(' BHK'),
+                                        ],
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.only(right: 8.0),
+                                        child: Text('2'),
+                                      ),
                                     ],
                                   ),
                                   Divider(thickness: 2),
                                   Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
                                     children: [
-                                      Icon(Icons.check_box_outline_blank, color: AppColors.black),
-                                      Text(' Carpet Area'),
-                                      SizedBox(width: 150),
+                                      Row(
+                                        children: [
+                                          Icon(
+                                            Icons.check_box_outline_blank,
+                                            color: AppColors.black,
+                                          ),
+                                          Text(' Carpet Area'),
+                                        ],
+                                      ),
+
                                       Text('1380 Sqft'),
                                     ],
                                   ),
                                   Divider(thickness: 1),
                                   Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
                                     children: [
-                                       Icon(Icons.bathtub, color: AppColors.black),
-                                      Text(' Bathrooms'),
-                                      SizedBox(width: 200),
-                                      Text(' 2'),
+                                      Row(
+                                        children: [
+                                          Icon(
+                                            Icons.bathtub,
+                                            color: AppColors.black,
+                                          ),
+                                          Text(' Bathrooms'),
+                                        ],
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.only(right: 10.0),
+                                        child: Text(' 2'),
+                                      ),
                                     ],
                                   ),
                                 ],
                               ),
                             ),
-                     SizedBox(height: 8),
-                         Text(
+                            SizedBox(height: 8),
+                            Text(
                               "Amenities",
-                              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                              ),
                             ),
-                        SizedBox(height: 8),
+                            SizedBox(height: 8),
                             // Amenities Rows
                             Row(
-                              children: [                               
-                                 Icon(Icons.directions_car, color: AppColors.black),
-                               SizedBox(width: 5),
-                                 Text(
+                              children: [
+                                Icon(
+                                  Icons.directions_car,
+                                  color: AppColors.black,
+                                ),
+                                SizedBox(width: 5),
+                                Text(
                                   'Car Parking',
-                                  style: TextStyle(fontSize: 15, color: AppColors.black),
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: AppColors.black,
+                                  ),
                                 ),
                               ],
                             ),
-                             SizedBox(height: 6),
+                            SizedBox(height: 6),
                             Row(
-                              children: [                               
-                                 Icon(Icons.currency_bitcoin, color: AppColors.black),
-                               SizedBox(width: 5),
-                             Text(
+                              children: [
+                                Icon(
+                                  Icons.currency_bitcoin,
+                                  color: AppColors.black,
+                                ),
+                                SizedBox(width: 5),
+                                Text(
                                   'Maintenance (Monthly) 2000',
-                                  style: TextStyle(fontSize: 15, color: AppColors.black),
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: AppColors.black,
+                                  ),
                                 ),
                               ],
                             ),
                           ],
                         ),
-                         SizedBox(height: 12),
+                        SizedBox(height: 12),
                         // --- Description Dropdown ---
                         ExpansionTile(
                           title: Text(
@@ -247,13 +307,13 @@ class _PropertydetailsScreenState extends State<PropertydetailsScreen> {
                               padding: EdgeInsets.only(bottom: 8.0),
                               child: Text(
                                 'Location : Karyavattom, Trivandrum \n(Near Technopark Greenfield stadium)\n'
-                               'Project : Impact Milestone-Premium residential Apartment\n'
+                                'Project : Impact Milestone-Premium residential Apartment\n'
                                 'Flat Type: 2 BHK Spacious Living & Dining\n'
                                 'Area : [insert Sq.Ft - Eg. 1050 Sq.Ft]\n'
                                 'car parking: Yes\n'
                                 'Amenities : Swimming Pool, Gym,\n 24*7 Security Power Backup\n'
                                 'Status : Ready To Move',
-                                style: TextStyle(fontSize: 15,),
+                                style: TextStyle(fontSize: 15),
                               ),
                             ),
                           ],
@@ -261,7 +321,7 @@ class _PropertydetailsScreenState extends State<PropertydetailsScreen> {
                       ],
                     ),
                   ),
-                 SizedBox(height: 10),
+                  SizedBox(height: 10),
                   Padding(
                     padding: EdgeInsets.only(bottom: 8.0),
                     child: Image.asset(
