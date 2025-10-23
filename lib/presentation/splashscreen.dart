a36a860a70f54899ebfaa1d2b23aacde229b0073
jafnafathima2004 <jafnafathima01@gmail.com>
2025-10-23 13:52:00 +0530

    splashscreen edited

--- lib/presentation/splashscreen.dart
+++ lib/presentation/splashscreen.dart
@@ -1,3 +1,59 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_svg/svg.dart';
+// import 'package:property_managment/core/theme/app_colors.dart';
+// import 'package:property_managment/core/theme/asset_resource.dart';
+
+// class Splashscreen extends StatefulWidget {
+//   const Splashscreen({super.key});
+
+//   @override
+//   State<Splashscreen> createState() => _SplashscreenState();
+// }
+
+// class _SplashscreenState extends State<Splashscreen> {
+// double _size = 0; // start from a point
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     // Start animation after a short delay
+//     Future.delayed(const Duration(milliseconds: 300), () {
+//       setState(() {
+//         _size = 200; // final size
+//       });
+//     });
+//     Future.delayed(const Duration(seconds: 8), () {
+//       if (!mounted) return;
+//     //   // Navigator.push(
+//     //   //   context,
+//     //   //   MaterialPageRoute(
+//     //   //     ,
+//     //   //   ),
+//     //   );
+//      });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor:AppColors.greenColor,
+//       body: Center(
+//         child: AnimatedContainer(
+//           duration: const Duration(seconds: 7),
+//           curve: Curves.easeOutBack, // smooth and bouncy growth
+//           width: _size,
+//           height: _size,
+//           decoration: BoxDecoration(borderRadius: BorderRadius.circular(50)),
+//           child:
+//               _size >
+//                   50 // show text only after it starts expanding
+//               ? Center(child: SizedBox(child:SvgPicture.asset(AssetResource.logo)))
+//               : null,
+//         ),
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:property_managment/core/theme/app_colors.dart';
@@ -11,45 +67,12 @@ class Splashscreen extends StatefulWidget {
 }
 
 class _SplashscreenState extends State<Splashscreen> {
-double _size = 0; // start from a point
-
-  @override
-  void initState() {
-    super.initState();
-    // Start animation after a short delay
-    Future.delayed(const Duration(milliseconds: 300), () {
-      setState(() {
-        _size = 200; // final size
-      });
-    });
-    Future.delayed(const Duration(seconds: 8), () {
-      if (!mounted) return;
-    //   // Navigator.push(
-    //   //   context,
-    //   //   MaterialPageRoute(
-    //   //     ,
-    //   //   ),
-    //   );
-     });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:AppColors.greenColor,
+      backgroundColor: AppColors.greenColor,
       body: Center(
-        child: AnimatedContainer(
-          duration: const Duration(seconds: 7),
-          curve: Curves.easeOutBack, // smooth and bouncy growth
-          width: _size,
-          height: _size,
-          decoration: BoxDecoration(borderRadius: BorderRadius.circular(50)),
-          child:
-              _size >
-                  50 // show text only after it starts expanding
-              ? Center(child: SizedBox(child:SvgPicture.asset(AssetResource.logo)))
-              : null,
-        ),
+        child: SvgPicture.asset(AssetResource.logo),
       ),
     );
   }
